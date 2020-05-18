50ef647de84630784f1b5294765f0f5142a82d89
Andreas Kessel <typo3-dev@formatsoft.de>
2020-05-18 08:00:52 +0200

    The page tsconfig should be loaded automatically. Otherwise the newContentElement wizard will not work.

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'ce_timeline',
-    'Configuration/TypoScript/TsConfig/page.typoscript',
-    'Timeline Content Element'
-);
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,6 @@
 <?php
+defined('TYPO3_MODE') || die();
+
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
    \TYPO3\CMS\Core\Imaging\IconRegistry::class
 );
@@ -7,3 +9,6 @@ $iconRegistry->registerIcon(
    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
    ['source' => 'EXT:ce_timeline/Resources/Public/Icons/content-timeline.svg']
 );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:ce_timeline/Configuration/TypoScript/TsConfig/page.typoscript">
+');
